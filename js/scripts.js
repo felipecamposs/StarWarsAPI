355ad09c67c8a586d70aa0e93ef05990ce1607cc
misurse <felipecamposdavid@gmail.com>
2022-08-10 01:25:17 -0300

    feat add atributos

--- js/scripts.js
+++ js/scripts.js
@@ -16,21 +16,55 @@ function getApi(url){
             })
         }
     )}
-        
 
 function getInfos(people){
     for (let i = 0; i < people.length; i++) {
-        var namePerson = people[i].name;
-
+        var data = {
+            name: people[i].name,
+            height: people[i].height,
+            hair_color: people[i].hair_color,
+            skin_color: people[i].skin_color,
+			eye_color: people[i].eye_color,
+			birth_year: people[i].birth_year,
+        }
+        
         var info = document.createElement("div")
         info.classList.add("info")
-        info.setAttribute("name", namePerson)
+        info.setAttribute("name", data.name)
         
-        let namePeople = document.createElement("div")
-        namePeople.classList.add("namePeople")
-        namePeople.innerHTML = `<span>${namePerson}</span>`
+        ///////
+        let name = document.createElement("span")
+        name.classList.add("name")
+        name.innerHTML = `Nome: ${data.name}`
 
-        info.appendChild(namePeople)
+        let height = document.createElement("span")
+        height.classList.add("height")
+        height.innerHTML = `Altura: ${data.height}`
+        
+        let hair_color = document.createElement("span")
+        hair_color.classList.add("hair_color")
+        hair_color.innerHTML = `Cabelo: ${data.hair_color}`
+
+        let skin_color = document.createElement("span")
+        skin_color.classList.add("skin_color")
+        skin_color.innerHTML = `Cor do cabelo: ${data.skin_color}`
+
+        let eye_color = document.createElement("span")
+        eye_color.classList.add("eye_color")
+        eye_color.innerHTML = `Cor olhos: ${data.eye_color}`
+
+        let birth_year = document.createElement("span")
+        birth_year.classList.add("birth_year")
+        birth_year.innerHTML = `Data de nascimento: ${data.birth_year}`
+        ///////
+
+        info.appendChild(name)
+        info.appendChild(height)
+        info.appendChild(hair_color)
+        info.appendChild(skin_color)
+        info.appendChild(eye_color)
+        info.appendChild(birth_year)
+
         let container = document.querySelector(".container")
         container.appendChild(info)
         
