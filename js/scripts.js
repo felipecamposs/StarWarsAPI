6e56f1ba73b3c70e9164a559ba1bbcb37f0291df
misurse <felipecamposdavid@gmail.com>
2022-08-20 18:16:44 -0300

    feat listAll, button Todos

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ const container = document.querySelector(".container")
 const params = new URLSearchParams(window.location.search)
 const page = params.get('page')
 const url = `https://swapi.dev/api/people`
+const btnAll = document.querySelector('#btn-all')
 var linkNext = ''
 var linkPrevious = ''
+var contagem = ''
 
 function getApi(url){
     let fetchConfig = {
@@ -19,6 +21,84 @@ function getApi(url){
         }
     )}
 
+function getApiAll(urlid){
+        let fetchConfig = {
+            method: "GET"
+        }
+    
+        fetch(urlid, fetchConfig)
+            .then(response => {
+                response.json()
+                .then(people => {
+                    listAll(people)
+                })
+            }
+        )}    
+
+function listAllBtn(){
+    container.innerHTML = ''
+    
+    for (personagemID = 1; personagemID <= contagem; personagemID++) {
+        let urlid = `https://swapi.dev/api/people/${personagemID}`
+        getApiAll(urlid)
+        } 
+        
+    }
+
+
+btnAll.addEventListener('click', listAllBtn)
+
+function listAll(people){
+    var data = {
+        name: people.name,
+        height: people.height,
+        hair_color: people.hair_color,
+        skin_color: people.skin_color,
+        eye_color: people.eye_color,
+        birth_year: people.birth_year
+    }
+    
+    var info = document.createElement("div")
+    info.classList.add("info")
+    info.setAttribute("name", data.name)
+    
+    ///////
+    let name = document.createElement("span")
+    name.classList.add("name")
+    name.innerHTML = `Nome: ${data.name}`
+    
+    let height = document.createElement("span")
+    height.classList.add("height")
+    height.innerHTML = `Altura: ${data.height}`
+    
+    let hair_color = document.createElement("span")
+    hair_color.classList.add("hair_color")
+    hair_color.innerHTML = `Cabelo: ${data.hair_color}`
+    
+    let skin_color = document.createElement("span")
+    skin_color.classList.add("skin_color")
+    skin_color.innerHTML = `Cor do cabelo: ${data.skin_color}`
+    
+    let eye_color = document.createElement("span")
+    eye_color.classList.add("eye_color")
+    eye_color.innerHTML = `Cor olhos: ${data.eye_color}`
+    
+    let birth_year = document.createElement("span")
+    birth_year.classList.add("birth_year")
+    birth_year.innerHTML = `Data de nascimento: ${data.birth_year}`
+    ///////
+    
+    info.appendChild(name)
+    info.appendChild(height)
+    info.appendChild(hair_color)
+    info.appendChild(skin_color)
+    info.appendChild(eye_color)
+    info.appendChild(birth_year)
+    
+    let container = document.querySelector(".container")
+    container.appendChild(info)     
+}
+
 function setButtons(people){
     
     var returnAPI = {
@@ -28,12 +108,12 @@ function setButtons(people){
         results: people.results
     }
     
+    contagem = returnAPI.count
     let htmlButtons = document.querySelector(".buttons")
     
     linkNext = returnAPI.next
     linkPrevious = returnAPI.previous
     
-    console.log(linkNext, linkPrevious)
     htmlButtons.innerHTML = '<input type="button" value="next" id="linkNext" onclick="getApi(linkNext);cleanContainer();"> <br> <input type="button" value="previous" id="linkPrevious" onclick="getApi(linkPrevious);cleanContainer();">'
 
     if (linkNext === null ){
@@ -97,39 +177,13 @@ function getInfos(people){
         info.appendChild(birth_year)
 
         let container = document.querySelector(".container")
-        container.appendChild(info)
-        
+        container.appendChild(info)     
     } 
 }
-
-//// FAZER TODOS --- 
-// function listAllPeople(){
-//     let container = document.querySelector('.container')
-//     container.innerHTML = ''
-    
-//     for (let i = 1; i < 10; i++) {
-//         num = i
-//         let urlTodos = `https://swapi.dev/api/people/?page=${num}`
-//         getApi(urlTodos)
-//     }
-// }
 
 function cleanContainer(){
     let container = document.querySelector('.container')
     container.innerHTML = ''
-}
-
-const search = document.querySelector('#search');
-search.addEventListener('input', filterSearch);
-    
-
-function filterSearch(){
-    const info = container.querySelectorAll('.info');
-
-    info.forEach((people) => {
-        let text = people.getAttribute("name");
-        console.log(text)
-    });
 }
 
 getApi(url)
